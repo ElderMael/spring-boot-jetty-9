2ee90e6872a973297d9c37b3e8b1867f582ef388
ElderMael <sftwr.mael@gmail.com>
2014-08-16 09:45:58 -0500

    Create Thymeleaf basic templates and change index page

--- src/main/java/mx/gob/jgtjo/web/controllers/IndexController.java
+++ src/main/java/mx/gob/jgtjo/web/controllers/IndexController.java
@@ -2,29 +2,21 @@ package mx.gob.jgtjo.web.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
-
-import static com.google.common.collect.Maps.newHashMap;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@RestController
-@RequestMapping(value = "/", produces = "application/json")
+@Controller
 public class IndexController {
 
     private static final Logger log = LoggerFactory
             .getLogger(IndexController.class);
 
     @RequestMapping(method = GET)
-    public Map<String, Object> show() {
-        Map<String, Object> response = newHashMap();
-        response.put("hello", "World!");
-
-        log.info("Hello, World!");
-
-        return response;
+    public ModelAndView index() {
+        return new ModelAndView("main/index");
     }
 
 }
