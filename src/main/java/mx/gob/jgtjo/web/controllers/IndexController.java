afff893ed6ee8b0d61809d74b245554f22e26b93
ElderMael <sftwr.mael@gmail.com>
2014-08-17 02:33:55 -0500

    Remove Thymeleaf namespaces on index page and add friendly message

--- src/main/java/mx/gob/jgtjo/web/controllers/IndexController.java
+++ src/main/java/mx/gob/jgtjo/web/controllers/IndexController.java
@@ -3,12 +3,12 @@ package mx.gob.jgtjo.web.controllers;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
 import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -21,11 +21,13 @@ public class IndexController {
             .getLogger(IndexController.class);
 
     @RequestMapping(method = GET)
-    public ModelAndView index(@AuthenticationPrincipal Principal principal) {
+    public ModelAndView index(@AuthenticationPrincipal User principal) {
 
         Map<String, Object> model = Maps.newHashMap();
 
         log.info("Hello, {}!", principal);
+
+        model.put("principal", principal.getUsername());
 
         return new ModelAndView("main/index", model);
     }
